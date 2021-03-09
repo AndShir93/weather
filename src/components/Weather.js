0ef7f8eae2a88da0fc3a9565e5de711d69089414
AndShir93 <andshir93@gmail.com>
2021-03-09 11:57:21 +0400

    mobile version

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCity, getWeather } from '../asyncActions/getWeather'
-import ArrowWrap from './ArrowWrap'
 
 export default function Weather(){
     const dispatch = useDispatch()
