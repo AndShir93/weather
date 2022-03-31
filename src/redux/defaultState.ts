bd9d3349d66b55aab86c571811e39567e8ddf4b1
AndShir93 <andshir93@gmail.com>
2022-04-01 01:28:12 +0400

    Перенес на TS

--- src/redux/defaultState.ts
+++ src/redux/defaultState.ts
@@ -1,7 +1,10 @@
-export const defaultState = {
+import {IState} from "../types/types";
+
+export const defaultState: IState = {
     city: 'Ижевск',
     temp: '',
     cityList: ['Izhevsk','Mozhga','Sarapul','London'],
     coords: {lat: 56.866558, lon: 53.209415},
-    wind: {deg: '', gust: '', speed: ''}
+    wind: {deg: '', gust: '', speed: ''},
+    description: '',
 }
